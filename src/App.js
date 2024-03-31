7fb07cacbfb89a33f7bf0edbc1d216ae249c7cbc
FriedhelmWS <352063465@qq.com>
2024-04-01 07:27:06 +0800

    init backend

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [inputText, setInputText] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("paste", (e) => {
+      const text = e.clipboardData.getData("text");
+      if (
+        text.startsWith("private") ||
+        text.startsWith("public") ||
+        text.startsWith("class")
+      ) {
+      }
+      setInputText(text);
+    });
+
+    return () => {
+      window.removeEventListener("paste", null);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="text-left whitespace-pre-wrap">{inputText}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
