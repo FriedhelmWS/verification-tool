b443911df75d3c0d2e77edb05549c13350e97954
FriedhelmWS <352063465@qq.com>
2024-04-02 03:44:51 +0800

    Add basic workflow

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const fs = require("fs");
+var cors = require("cors");
 
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, (error) => {
   if (!error)
@@ -12,7 +16,33 @@ app.listen(PORT, (error) => {
   else console.log("Error occurred, server can't start", error);
 });
 
-app.get("/hello", (req, res) => {
-  res.set("Content-Type", "text/html");
-  res.status(200).send("<h1>Hello GFG Learner!</h1>");
+app.post("/validate", (req, res) => {
+  const code = req.body.code;
+  const file = "test.java";
+
+  fs.writeFileSync(file, code);
+
+  res.set("Content-Type", "application/json");
+  res.status(200).send(
+    JSON.stringify({
+      issue: `
+        test.java:4: error: Null Dereference
+        object \`s\` last assigned on line 3 could be null and is dereferenced at line 4.
+        2.     int test() {
+        4. >     return s.length();
+        5.     }
+        6.   }
+    `,
+    })
+  );
+});
+
+app.post("/correct", (req, res) => {
+  const code = req.body.code;
+  const issue = req.body.issue;
+
+  const prompt = `${code}\n This piece of code has the following issue(s): \n ${issue} \n Correct this.`;
+
+  res.set("Content-Type", "application/json");
+  res.status(200).send(JSON.stringify({ prompt: prompt }));
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./App.css";
 
+const baseUrl = "http://localhost:3001/";
+
 function App() {
   const [inputText, setInputText] = useState(false);
+  const [issue, setIssue] = useState("");
 
   useEffect(() => {
     window.addEventListener("paste", (e) => {
       const text = e.clipboardData.getData("text");
-      if (
-        text.startsWith("private") ||
-        text.startsWith("public") ||
-        text.startsWith("class")
-      )
-        setInputText(text);
+      setInputText(text);
+
+      axios
+        .post(
+          baseUrl + "validate",
+          { code: text },
+          {
+            headers: { "Access-Control-Allow-Origin": true },
+          }
+        )
+        .then(function (response) {
+          setIssue(response.data.issue);
+        });
     });
 
     return () => {
@@ -24,7 +35,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="whitespace-pre-wrap text-left">{inputText}</div>
+        <div className="text-left whitespace-pre-wrap">{inputText}</div>
+        <div className="text-left text-red-600 whitespace-pre-wrap">
+          {issue}
+        </div>
+        <button
+          className="bg-green-800"
+          onClick={() => {
+            axios
+              .post(
+                baseUrl + "correct",
+                { code: inputText, issue: issue },
+                {
+                  headers: { "Access-Control-Allow-Origin": true },
+                }
+              )
+              .then(function (response) {
+                navigator.clipboard.writeText(response.data.prompt);
+              });
+          }}
+        >
+          Correct It
+        </button>
       </header>
     </div>
   );
