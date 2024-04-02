cf359b991ac3c8fe2eb02552327e588ca38f96f3
FriedhelmWS <352063465@qq.com>
2024-04-03 03:24:34 +0800

    Set up diff showing

--- backend/test.java
+++ backend/test.java
@@ -1,6 +1,11 @@
 class Hello {
   int test() {
     String s = null;
-    return s.length();
+    if (s != null) {  // Check if s is not null
+      return s.length();
+    } else {
+      // Handle the case where s is null
+      return 0; // Or throw an exception
+    }
   }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import ReactDiffViewer from "react-diff-viewer";
 
 import "./App.css";
 
 const baseUrl = "http://localhost:3001/";
 
 function App() {
-  const [inputText, setInputText] = useState(false);
+  const [inputText, setInputText] = useState("");
+  const [correctText, setCorrectText] = useState("");
   const [issue, setIssue] = useState("");
 
   useEffect(() => {
@@ -35,28 +37,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="text-left whitespace-pre-wrap">{inputText}</div>
+        <ReactDiffViewer
+          oldValue={inputText}
+          newValue={correctText}
+          splitView={true}
+        />
         <div className="text-left text-red-600 whitespace-pre-wrap">
           {issue}
         </div>
-        <button
-          className="bg-green-800"
-          onClick={() => {
-            axios
-              .post(
-                baseUrl + "correct",
-                { code: inputText, issue: issue },
-                {
-                  headers: { "Access-Control-Allow-Origin": true },
-                }
-              )
-              .then(function (response) {
-                navigator.clipboard.writeText(response.data.prompt);
-              });
+
+        <div className="flex gap-10">
+          <button
+            className="px-5 bg-green-600 rounded-full"
+            onClick={() => {
+              axios
+                .post(
+                  baseUrl + "correct",
+                  { code: inputText, issue: issue },
+                  {
+                    headers: { "Access-Control-Allow-Origin": true },
+                  }
+                )
+                .then(function (response) {
+                  navigator.clipboard.writeText(response.data.prompt);
+                });
+            }}
+          >
+            Correct It
+          </button>
+          <button className="px-5 bg-blue-600 rounded-full">Reset</button>
+        </div>
+        <textarea
+          onChange={(e) => {
+            e.stopPropagation();
+            setCorrectText(e.target.value);
           }}
-        >
-          Correct It
-        </button>
+        />
       </header>
     </div>
   );
